d7736f24f75e5fdc6ed3b9595bae2173066bfa32
Tong XUE <xtstella@hotmail.com>
2022-05-16 12:45:42 +0200

    feat: add Table base component

---
+++ components/base/Table.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-if="rows" class="w-full">
+    <table class="w-full bg-white">
+      <thead>
+        <tr>
+          <th
+            v-for="(header, i) in headers"
+            :key="i"
+            class="cursor-pointer text-left text-bluescale-200 text-body-median font-bold px-5 py-4 border-bluescale-400"
+            :class="header.text ? 'border-b' : 'border-0'"
+          >
+            <span class="whitespace-nowrap">{{ header.text }}</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, i) in rows" :key="i">
+          <td
+            v-for="header in headers"
+            :key="header.value"
+            class="px-5 py-5 font-light text-bluescale-200 text-body-median"
+          >
+            <span class="whitespace-nowrap">{{ row[header.value] }}</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      default: () => [],
+    },
+    rows: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style></style>
